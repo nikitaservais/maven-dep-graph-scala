a0d780d98a71ed910a6b54e05b0f15579695a7e6
nservais <nikita@lab-box.studio>
2023-11-23 19:15:11 +0100

    feat: finish countDependencies flow

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,43 @@
 import akka.actor.ActorSystem
-import akka.stream.scaladsl._
+import akka.stream.{FlowShape, Graph}
+import akka.stream.scaladsl.*
 import akka.util.ByteString
+
 import java.nio.file.Paths
 import concurrent.duration.DurationInt
 
 val MAX_SUBSTREAMS = 185
 val GroupsPerSecond = 10 // Throttle rate
 
-case class MavenDependency(library: Library, dependency: Dependency, dependencyType: DependencyType)
+case class MavenDependency(library: Library, dependency: Dependency)
 
 case class Library(GroupId: String, ArtifactId: String, Version: String)
 
-case class Dependency(GroupId: String, ArtifactId: String, Version: String);
+case class Dependency(GroupId: String, ArtifactId: String, Version: String, DependencyType: DependencyType);
+
+case class MavenLibrary(library: Library, dependencies: List[Dependency], compileCount: Int, runtimeCount: Int) {
+  override def toString: String = {
+    val numberOfCharacters = library.GroupId.length + library.ArtifactId.length + library.Version.length
+    val numberOfSpaces = 65 - numberOfCharacters
+    s"Name: ${library.GroupId} ${library.ArtifactId} ${library.Version} ${" " * numberOfSpaces}--> Compile: ${compileCount}${" " * (3 - compileCount.toString.length)} Runtime: ${runtimeCount}"
+  }
+}
 
 enum DependencyType {
   case COMPILE, RUNTIME
+}
+
+def parseLine(line: String): MavenDependency = {
+  val parts = line.split(",")
+  val libraryParts = parts(0).split(":")
+  val dependencyParts = parts(1).split(":")
+  val library = Library(libraryParts(0), libraryParts(1), libraryParts(2))
+  val dependencyType = parts(2) match {
+    case "Compile" => DependencyType.COMPILE
+    case "Runtime" => DependencyType.RUNTIME
+  }
+  val dependency = Dependency(dependencyParts(0), dependencyParts(1), dependencyParts(2), dependencyType)
+  MavenDependency(library, dependency)
 }
 
 object Main extends App {
@@ -27,33 +50,48 @@ object Main extends App {
     .drop(1)
 
   val instantiateClasses = Flow[String].map(parseLine)
+  // this will groups by Library, so the output is Library, List(Dependency)
+  val groupByLibraryName = Flow[MavenDependency].groupBy(MAX_SUBSTREAMS, _.library)
+    .fold(MavenLibrary(null, List(), 0, 0))((acc, value) => {
+      if (acc.library == null) {
+        MavenLibrary(value.library, List(value.dependency), 0, 0)
+      } else {
+        MavenLibrary(value.library, acc.dependencies :+ value.dependency, 0, 0)
+      }
+    })
+    .mergeSubstreams
 
-  val groupByLibraryName = Flow[MavenDependency].groupBy(MAX_SUBSTREAMS, _.library).mergeSubstreams
+  val throttle = Flow[MavenLibrary].throttle(GroupsPerSecond, 1.second)
 
-  val throttle = Flow[MavenDependency].throttle(GroupsPerSecond, 1.second)
+  val buffer = Flow[MavenLibrary].buffer(5, akka.stream.OverflowStrategy.backpressure)
 
-  val buffer = Flow[MavenDependency].buffer(5, akka.stream.OverflowStrategy.backpressure)
+  val countDependenciesGraph: Graph[FlowShape[MavenLibrary, MavenLibrary], akka.NotUsed] = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    // Define the stages
+    val broadcast = builder.add(Broadcast[MavenLibrary](2))
+    val countCompile = Flow[MavenLibrary].map(lib => lib.copy(compileCount = lib.dependencies.count(_.DependencyType == DependencyType.COMPILE)))
+    val countRuntime = Flow[MavenLibrary].map(lib => lib.copy(runtimeCount = lib.dependencies.count(_.DependencyType == DependencyType.RUNTIME)))
+    val zip = builder.add(ZipWith[MavenLibrary, MavenLibrary, MavenLibrary]((compileLib, runtimeLib) =>
+      MavenLibrary(compileLib.library, compileLib.dependencies, compileLib.compileCount, runtimeLib.runtimeCount)))
+
+    // Connect the stages
+    broadcast.out(0) ~> countCompile ~> zip.in0
+    broadcast.out(1) ~> countRuntime ~> zip.in1
+
+    FlowShape(broadcast.in, zip.out)
+  }
+
+  val countDependenciesFlow = Flow.fromGraph(countDependenciesGraph)
 
   val flow = parseFile.via(instantiateClasses)
     .via(groupByLibraryName)
     .via(throttle)
     .via(buffer)
-    .map(_.library)
+    .via(Flow.fromGraph(countDependenciesFlow)) // Using the custom Flow
+    .map(_.toString)
 
   val sink = Sink.foreach(println)
 
   source.via(flow).to(sink).run()
-}
-
-def parseLine(line: String): MavenDependency = {
-  val parts = line.split(",")
-  val libraryParts = parts(0).split(":")
-  val dependencyParts = parts(1).split(":")
-  val library = Library(libraryParts(0), libraryParts(1), libraryParts(2))
-  val dependency = Dependency(dependencyParts(0), dependencyParts(1), dependencyParts(2))
-  val dependencyType = parts(2) match {
-    case "Compile" => DependencyType.COMPILE
-    case "Runtime" => DependencyType.RUNTIME
-  }
-  MavenDependency(library, dependency, dependencyType)
 }
